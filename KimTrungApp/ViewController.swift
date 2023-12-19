f79e8c86616ead463679a387879236d353195967
hieuNT <hieunthe163816@fpt.edu.vn>
2023-12-19 16:08:35 +0700

    Initial Commit

---
+++ KimTrungApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KimTrungApp
+//
+//  Created by HieuNT on 19/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
